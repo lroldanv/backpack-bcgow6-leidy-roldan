4cf1d114e5c428ed4233556b4a8d859145730124
lroldan <leidy.roldan@mercadolibre.com.co>
2022-10-02 20:09:29 -0500

    Update coding practices relating to  functions

--- clase-2-1/exercise-1/main.go
+++ clase-2-1/exercise-1/main.go
@@ -1,29 +1,41 @@
 package main
 
-import ("fmt"
-		"errors"
+import (
+	"errors"
+	"fmt"
 )
 
-func calculateTaxes (salary float64) (float64, error) {
+var Errfoo error = errors.New("error: negative values are forbidden")
+
+func calculateTaxes(salary float64) (float64, error) {
 
 	if salary < 0 {
-		return 0, errors.New("Negative value")
-		err = error.
+		return 0, Errfoo
 	}
 
-	if salary > 50000 {
-		return salary * 0.17
-		
-	}else if salary > 150_000 {
-		return salary * 0.27
+	if salary > 50000.0 && salary <= 150000.0 {
+		fmt.Println("Taxes are applied (17%)")
+		taxes := salary * 0.17
+		return taxes, nil
 	}
-	return 0
+
+	if salary > 150000.0 {
+		fmt.Println("Taxes are applied (27%)")
+		taxes := salary * 0.27
+		return taxes, nil
+	}
+
+	return 0, nil
+
 }
 
-func main(){
-	salary := 78000.0
-	fmt.Println(calculateTaxes(salary))
+func main() {
+	salary := 90000.0
+
+	Taxes, err := calculateTaxes(salary)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(calculateTaxes(Taxes))
 
 }
-
-// Generar los errores de manera global???
--- clase-2-1/exercise-2/main.go
+++ clase-2-1/exercise-2/main.go
@@ -1,25 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
-func calculateAverage(values...float64) float64 {
-	var result float64
-	result = sum(values)/len(values)
-	return result
-	
-}
-
-
-func sum(values...float64) float64{
+func calculateAverage(values ...float64) (float64, error) {
 	var total float64
 	for _, value := range values {
+		if value < 0 {
+			return 0, errors.New("error: negative values")
+			os.Exit(1)
+		}
 		total += value
 	}
-	return total
+	average := total / float64(len(values))
+	return average, nil
 }
 
-func main()  {
-	fmt.Println(sum(2.0, 3.0, 4.0))
-	fmt.Println()
+func main() {
+	average, err := calculateAverage(23, 34, 45, 50)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(average)
+
 }
-// salarios y proemdios node deben ser negativos
